f7aac517fc09e6ceae4a0231938e59d6956db11e
Yera <ciclost@localhost>
2025-09-16 16:59:40 +0200

    Empezado problemas obligatorios

--- Vanilla/EJ2/library/function.js
+++ Vanilla/EJ2/library/function.js
@@ -4,16 +4,23 @@ let puntosTeamMarcos = [89,120,103]
 let puntosTeamJuan = [116,94, 123]
 let puntosTeamMaria = [97, 134, 105]
 
-function avarage(match1, match2, match3) {
+function showPoints (arrayPoints, team) {
+    let line;
+    for (let i = 0; arrayPoints.length; i++) {
+        if (i < arrayPoints.length - 1) line = line + toString(arrayPoints[i] + "-")
+        else line = line + toString(arrayPoints[i])
+    } 
+    return "Los puntos del equipo" + team + " son: " + line  
+}
+
+function average(match1, match2, match3) {
     if (!isNaN(match1) && !isNaN(match2) && !isNaN(match3))
         return (match1 + match2 + match3) / 2
     else return undefined
 }
 
-function compareAvarage() {
-    let teamMarcos = avarage(89, 120, 103)
-    let teamJuan = avarage(116, 94, 123)
-    let teamMaria = avarage(97, 134, 105)
+function compareAverage() {
+    
 
     if (teamMarcos > teamJuan && teamMarcos > teamMaria) return ()
 
---
+++ Vanilla/ProblemaObligatorio/EJ1/library/function.js
@@ -0,0 +1,20 @@
+"use strict"
+const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+]
+
+function showMonth(month) {
+    if (isNaN(month)) return "Eso no es un numero animal de corral"
+    if (Month < 1 || Month > 12) return "No es un numero de mes valido"
+}
