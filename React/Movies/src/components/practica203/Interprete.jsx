79f39faf22b669d0da3428de71932a7b0d36a6ca
Yera <ciclost@localhost>
2025-09-19 19:30:53 +0200

    Falta el punto 3 del 2.03

--- React/Movies/src/components/practica203/Interprete.jsx
+++ React/Movies/src/components/practica203/Interprete.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React from "react"
 import "./Interprete.css"
 
-const Interprete = () => {
-    return
+const Interprete = (props) => {
+    return (
+        <>
+            <div className="interprete_container">
+                <img
+                    src={props.foto}
+                    alt="imagen"
+                    className="interprete_image"
+                ></img>
+                <div className="interprete_texto">
+                    <h1 className="interprete_name">{props.nombre}</h1>
+                    <p className="interprete_description">{props.children}</p>
+                </div>
+            </div>
+        </>
+    )
 }
+
+export default Interprete
