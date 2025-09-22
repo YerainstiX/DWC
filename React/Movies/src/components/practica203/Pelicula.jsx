02156e686bc5dcf50f7d066a6c6331093f675524
YerainstiX <yerainsti04@gmail.com>
2025-09-22 09:46:15 +0200

    Terminado react 2.03 falta arreglar el css

---
+++ React/Movies/src/components/practica203/Pelicula.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import "./Pelicula.css"
+
+const Pelicula = (props) => {
+    return (
+        <>
+            <div className="pelicula_container">
+                <img
+                    src={props.cartelera}
+                    alt="imagen"
+                    className="pelicula_image"
+                ></img>
+                <div className="pelicula_texto">
+                    <h1 className="plicula_titulo">{props.titulo}</h1>
+                    <h2 className="pelicula_direccion"></h2>
+                    <p className="pelicula_resumen">{props.children}</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Pelicula
