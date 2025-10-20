b62fc46f3786217d4d810a2902dc4cc9f9158c3c
YerainstiX <yerainsti04@gmail.com>
2025-10-20 13:36:42 +0200

    me cago en la puta componente terminado

--- React/Movies/src/components/2_03/Pelicula.jsx
+++ React/Movies/src/components/2_03/Pelicula.jsx
@@ -5,14 +5,16 @@ const Pelicula = (props) => {
     return (
         <>
             <div className="pelicula_container">
-                <img
-                    src={props.cartelera}
-                    alt="imagen"
-                    className="pelicula_image"
-                ></img>
                 <div className="pelicula_texto">
                     <h1 className="plicula_titulo">{props.titulo}</h1>
                     <h2 className="pelicula_direccion">{props.direccion}</h2>
+                </div>
+                <div className="pelicula_image_resumen">
+                    <img
+                        src={props.cartelera}
+                        alt="imagen"
+                        className="pelicula_image"
+                    ></img>
                     <p className="pelicula_resumen">{props.children}</p>
                 </div>
             </div>
---
+++ React/Movies/src/components/4_05/ListaInterpretes.jsx
@@ -0,0 +1,28 @@
+import React, { useRef } from "react";
+import Interprete from "../2_03/Interprete";
+
+ const ListaInterpretes = ({ interpretes }) => {
+    const contenedorRef = useRef(null);
+
+    const toggleInterpretes = () => {
+        if (contenedorRef.current) {
+            contenedorRef.current.style.display =
+                contenedorRef.current.style.display === "none" ? "flex" : "none";
+        }
+    };
+
+    return (
+        <div className="ListaInterprete_container">
+            <button onClick={toggleInterpretes}>Intérpretes</button>
+            <div ref={contenedorRef} className="ListaInterprete_oculto">
+                {interpretes.map((int, index) => (
+                    <Interprete key={index} nombre={int.nombre} foto={int.foto}>
+                        {int.descripcion}
+                    </Interprete>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default ListaInterpretes
