b9fe07745ead1dbaa2c0acfe5c9bb75b519e8909
Yera <ciclost@localhost>
2025-10-31 20:38:06 +0100

    casi done

--- Vanilla/UT5/5_03/main.js
+++ Vanilla/UT5/5_03/main.js
@@ -6,20 +6,56 @@ window.onload = () => {
 
     const container = document.querySelector(".colors")
     const canva = document.querySelector(".canva")
+    const td = document.querySelectorAll("td")
+    const remove = document.querySelector(".remove_color")
+    let clase = ""
+    let painting = false
 
+    //SELECT COLOR
     container.addEventListener(
         "click",
         (event) => {
-            const clase = container.children.event.target.classList
+            clase = event.target.classList.value
+            document.querySelector(".color_select").innerText = clase
+        },
+        false
+    )
+
+    //CLEAR CANVA
+    remove.addEventListener(
+        "click",
+        (event) => {
+            Array.from(td).forEach((e) => (e.className = ""))
+        },
+        false
+    )
+
+    //PAINT
+    canva.addEventListener(
+        "mousedown",
+        (event) => {
+            if (event.target.tagName === "TD") {
+                event.target.className = clase
+                painting = true
+            }
         },
         false
     )
 
     canva.addEventListener(
-        "click",
+        "mouseup",
+        () => {
+            painting = false
+        },
+        false
+    )
+
+    canva.addEventListener(
+        "mouseover",
         (event) => {
-            event.target.classList = []
-            event.target.classList.add(clase)
+            if (event.target.tagName === "TD" && painting) {
+                event.target.className = clase
+            }
         },
         false
     )
