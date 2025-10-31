04b773cf7db5b56574af3736be556c4a1a6edbd2
YerainstiX <yerainsti04@gmail.com>
2025-10-31 14:24:39 +0100

    terminado el 5_02

--- Vanilla/UT5/5_02/library/ex1.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-const show = () => {
-    document.getElementsByClassName("container_ex1")
-}
-
-const hide = () => {
-
-}
--- Vanilla/UT5/5_02/main.js
+++ Vanilla/UT5/5_02/main.js
@@ -1,14 +1,42 @@
 "use strict"
 
 window.onload = () => {
+    //EX1
     const container = document.querySelector(".container_ex1")
 
     container.addEventListener(
         "click",
         (event) => {
             if (event.target.classList.contains("odd")) {
-                event.target.nextElementSibling.classList.contains("oculto")
+                const sibling = event.target.nextElementSibling
+
+                if (sibling && sibling.classList.contains("even")) {
+                    sibling.classList.toggle("hidden")
+                }
             }
+        },
+        false
+    )
+
+    //===================================================================
+    //EX2
+
+    const tabs = document.querySelector(".tabs")
+    const content = document.querySelector(".content")
+
+    tabs.addEventListener(
+        "click",
+        (event) => {
+            //I get the index of the tab where I'm clicking
+            const index = [...tabs.children].indexOf(event.target)
+
+            //I apply the class hidden to every element of content
+            Array.from(content.children).forEach((element) => {
+                element.classList.add("hidden")
+            })
+
+            //I remove the class hidden from the element corresponding with the tab
+            content.children[index].classList.remove("hidden")
         },
         false
     )
