b5ab64c1f5e479c97d62787bece5420b0be2e6d9
Yera <ciclost@localhost>
2025-11-04 17:41:00 +0100

    TODO: logica de tablero ganador

--- Vanilla/UT5/5_04/lib/ex1.js
+++ Vanilla/UT5/5_04/lib/ex1.js
@@ -1,19 +1,28 @@
 "use strict"
 
-const images = Array.from(
-    "../assets/img1",
-    "../assets/img2",
-    "../assets/img3",
-    "../assets/img4",
-    "../assets/img5",
-    "../assets/img6",
-    "../assets/img7",
-    "../assets/img8",
-    "../assets/img9"
-)
+export const generatePieces = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        let url = array[i]
 
-export const shuffleArray = () => {
-    for (let i = 0; i < 9; i++) {
-        const element = array[i]
+        let img = document.createElement("img")
+        img.src = url
+        img.alt = `Trozo`
+        img.id = i
+        img.setAttribute("draggable", true)
+
+        document.getElementById("draggables").appendChild(img)
     }
+}
+
+//Fisher–Yates algorism to shuffle the array
+export const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+
+        let aux = array[i]
+
+        array[i] = array[j]
+        array[j] = aux
+    }
+    return array
 }
--- Vanilla/UT5/5_04/main.js
+++ Vanilla/UT5/5_04/main.js
@@ -1,2 +1,63 @@
 "use strict"
 
+import { generatePieces, shuffle } from "./lib/ex1.js"
+
+window.onload = () => {
+    const images = [
+        "./assets/img1.png",
+        "./assets/img2.png",
+        "./assets/img3.png",
+        "./assets/img4.png",
+        "./assets/img5.png",
+        "./assets/img6.png",
+        "./assets/img7.png",
+        "./assets/img8.png",
+        "./assets/img9.png",
+    ]
+
+    let shuffledArray = shuffle(images)
+    generatePieces(shuffledArray)
+
+    document.getElementById("draggable_zone").addEventListener(
+        "dragstart",
+        (evento) => {
+            evento.dataTransfer.setData("id", evento.target.id)
+            evento.dataTransfer.setData("name", evento.target.localName)
+        },
+        false
+    )
+
+    document.getElementById("draggable_zone").addEventListener(
+        "dragover",
+        (evento) => {
+            evento.preventDefault()
+        },
+        false
+    )
+
+    document.getElementById("draggable_zone").addEventListener(
+        "drop",
+        (evento) => {
+            evento.preventDefault()
+            if (evento.target.classList.contains("releasable")) {
+                evento.target.appendChild(
+                    document.getElementById(evento.dataTransfer.getData("id"))
+                )
+            }
+        },
+        false
+    )
+
+    document.getElementById("btt1").addEventListener(
+        "click",
+        () => {
+            const releasable = document.getElementsByClassName("releasable")
+            for (let i = 0; i < releasable.length; i++) {
+                releasable[i].innerHTML = ""
+            }
+            shuffledArray = shuffle(images)
+            generatePieces(shuffledArray)
+        },
+        false
+    )
+} //THE END
