f5ba94dc38695cb3f40e886eeec3bb816a85c58b
Yera <ciclost@localhost>
2026-01-19 19:00:27 +0100

    nose

---
+++ React/UglyShop/src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Footer.css"
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                © 2025 Site for ugly people . All right reserved except if you
+                are handsome
+            </footer>
+        </>
+    )
+}
+
+export default Footer
